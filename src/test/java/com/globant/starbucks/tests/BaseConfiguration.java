93d6bf743002b975d147d4329b92e6ea2213e8f4
Luis Martinez <luisfmp@gmail.com>
2016-06-29 16:15:49 -0500

    Version final de metodo para screenshot on error

--- src/test/java/com/globant/starbucks/tests/BaseConfiguration.java
+++ src/test/java/com/globant/starbucks/tests/BaseConfiguration.java
@@ -3,16 +3,22 @@
  */
 package com.globant.starbucks.tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 
 /**
  * Contiene la configuracion general de los tests
@@ -66,6 +72,17 @@ public class BaseConfiguration {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+			String filePath = ".\\" + result.getInstanceName() + "." + "ScreenShot.jpg";
+			System.out.println("===================================" + filePath);
+			FileUtils.copyFile(scrFile, new File(filePath));
 		}
 	}
 
--- src/test/java/com/globant/starbucks/tests/OrderingTest.java
+++ src/test/java/com/globant/starbucks/tests/OrderingTest.java
@@ -1,23 +1,15 @@
 package com.globant.starbucks.tests;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.globant.starbucks.pages.Home;
 import com.globant.starbucks.pages.menues.Tea;
 
-public class OrderingTest extends BaseConfiguration {
+public class OrderingTest extends BaseConfiguration{
 
 	private Home home;
 
@@ -31,9 +23,9 @@ public class OrderingTest extends BaseConfiguration {
 	public void tearDown() {
 		tearDownDriver();
 	}
-
+	
 	@Test
-	public void shouldOrdenateCorrectly() {
+	public void shouldOrdenateCorrectly(){
 		home.goHome();
 		Tea tea = home.goToTeaPageThroughImage();
 		tea.toggleFilterByTitle("Teavana");
@@ -42,16 +34,5 @@ public class OrderingTest extends BaseConfiguration {
 		tea.sortByVisibleText("Price - High To Low");
 		String firstProductName = tea.findFirstProductInList().getProductName();
 		Assert.assertEquals(lastProductName, firstProductName, "Los Nombres no son iguales");
-	}
-
-	@AfterMethod
-	public void afterMethod(ITestResult result) throws IOException {
-		System.out.println("===================================" + result.getTestName() + "---" + result.getStatus());
-		if (result.getStatus() == ITestResult.FAILURE) {
-			File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-			String filePath = ".\\" + this.getClass().getName() + "ScreenShot.jpg";
-			System.out.println("===================================" + filePath);
-			FileUtils.copyFile(scrFile, new File(filePath));
-		}
 	}
 }
