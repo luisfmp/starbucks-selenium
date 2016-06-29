efa7ae067469568e57786405de4e0198037a15dd
Luis Martinez <luisfmp@gmail.com>
2016-06-29 14:42:36 -0500

    Se agrega metodo para screenshot

--- src/test/java/com/globant/starbucks/tests/BaseConfiguration.java
+++ src/test/java/com/globant/starbucks/tests/BaseConfiguration.java
@@ -3,16 +3,22 @@
  */
 package com.globant.starbucks.tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 
 /**
  * Contiene la configuracion general de los tests
@@ -68,6 +74,14 @@ public class BaseConfiguration {
 			}
 		}
 
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File(".\\" + this.getClass().getName() + "ScreenShot.jpg"));
+		}
 	}
 
 }
