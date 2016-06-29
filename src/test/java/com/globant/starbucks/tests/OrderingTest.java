5fe13015514e78f175efa4d793a95a44ab6d1518
Luis Martinez <luisfmp@gmail.com>
2016-06-29 15:42:00 -0500

    Pruebas de screenshot

--- src/test/java/com/globant/starbucks/tests/OrderingTest.java
+++ src/test/java/com/globant/starbucks/tests/OrderingTest.java
@@ -1,15 +1,23 @@
 package com.globant.starbucks.tests;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.globant.starbucks.pages.Home;
 import com.globant.starbucks.pages.menues.Tea;
 
-public class OrderingTest extends BaseConfiguration{
+public class OrderingTest extends BaseConfiguration {
 
 	private Home home;
 
@@ -23,9 +31,9 @@ public class OrderingTest extends BaseConfiguration{
 	public void tearDown() {
 		tearDownDriver();
 	}
-	
+
 	@Test
-	public void shouldOrdenateCorrectly(){
+	public void shouldOrdenateCorrectly() {
 		home.goHome();
 		Tea tea = home.goToTeaPageThroughImage();
 		tea.toggleFilterByTitle("Teavana");
@@ -34,5 +42,15 @@ public class OrderingTest extends BaseConfiguration{
 		tea.sortByVisibleText("Price - High To Low");
 		String firstProductName = tea.findFirstProductInList().getProductName();
 		Assert.assertEquals(lastProductName, firstProductName, "Los Nombres no son iguales");
+	}
+
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			File scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+			String filePath = ".\\" + this.getClass().getName() + "ScreenShot.jpg";
+			System.out.println("===================================" + filePath);
+			FileUtils.copyFile(scrFile, new File(filePath));
+		}
 	}
 }
